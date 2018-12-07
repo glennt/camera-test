059399ef75ab4c2a740d18f2d12c04c3e48360c5
glennt <gtolenti@gmail.com>
2018-12-06 19:21:27 -0800

    Added previews for image and video

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import autoBind from "react-autobind";
+import RecordRTC from "recordrtc";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		autoBind(this);
+		this.state = {
+			recording: false
+		}
+	}
+
+	takeScreenshot() {
+		const imageSrc = this.webcam.getScreenshot();
+
+		this.setState({
+			mode: "image",
+			imagePreview: imageSrc
+		})
+	}
+
+	startRecording() {
+		let stream = this.webcam.stream;
+
+		let recorder = new RecordRTC(stream, { type: 'video' });
+		recorder.startRecording();
+
+		this.setState({
+			mode: "video",
+			recorder: recorder,
+			recording: true
+		})
+	}
+
+	stopRecording() {
+		let recorder = this.state.recorder;
+
+		recorder.stopRecording(() => {
+
+			this.setState({
+				recording: false,
+				videoPreviewUrl: recorder.toURL()
+			})
+
+		});
+	}
+
+	render() {
+
+		let imagePreview;
+		if (this.state.imagePreview && this.state.mode === "image") {
+			imagePreview = <img src={this.state.imagePreview}></img>;
+		}
+
+		let videoPreview;
+		if (this.state.videoPreviewUrl && this.state.mode === "video") {
+			videoPreview = <video src={this.state.videoPreviewUrl} controls />;
+		}
+
+		let startRecordingButton = (this.state.recording === false) ? <button onClick={this.startRecording}>Start recording</button> : null;
+		let stopRecordingButton = (this.state.recording) ? <button onClick={this.stopRecording}>Stop recording</button> : null;
+
+		return (
+			<div className="App">
+				<div>
+					<div>
+						<Webcam ref={e => this.webcam = e} />
+					</div>
+					<button onClick={this.takeScreenshot}>Take Screenshot</button>
+					{startRecordingButton}
+					{stopRecordingButton}
+				</div>
+				<div>
+					<p>Preview</p>
+					{imagePreview}
+					{videoPreview}
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default App;
